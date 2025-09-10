27d6d080ff8f1a2f9fc35358fe54108103a5373c
cwdiller <cwdiller@willamette.edu>
2025-09-09 23:26:39 -0700

    Karel_Painting.py

--- Karel_Painting.py
+++ Karel_Painting.py
@@ -14,6 +14,39 @@
 
 import karel
 
+
 def main():
-    """ Function to cause Karel to paint 3 sides of its house and then go indoors. """
-    # You can add your code below
+    turn_around()
+
+    while not_facing_north(): #if karel is not facing north, she will:
+        if left_is_blocked(): #paint the wall if its on her left, otherwise she:
+            paint()
+        else:                 # will turn to the left and move forward one, and if there is a wall on her left she will paint it
+            turn_left()
+            move()
+    if facing_north(): # if karel is facing north, she will go inside
+        go_inside()
+
+def paint():
+    while left_is_blocked(): # if there is a wall on karel's left, she will:
+        put_beeper() # paint the wall
+        move() # move forward
+
+
+def turn_right():
+    for c in range(3): # c is the greatest letter
+        turn_left() # one right is three lefts of course
+
+     
+def turn_around():
+    for c in range(2): # turns around
+        turn_left()
+
+def go_inside():
+    
+    while facing_north():      # if karel is facing north, she will:
+        if left_is_blocked(): # move along the wall until she reaches the door, where she will:
+            move()
+        else:                 # turn toward the door and go inside
+            turn_left()
+            move()
